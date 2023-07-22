94a9eb257645d6f48fe28b969b1fede5eacbae1d
mohan_2231 <mahan.singh.tomar1034@gmail.com>
2023-07-22 12:29:10 +0530

    use of ejs template

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = " ";
+
+  switch (currentDay) {
+    case 0:
+      day = "sunday";
+      break;
+    case 1:
+      day = "monday";
+      break;
+    case 2:
+      day = "tuesday";
+      break;
+    case 3:
+      day = "wednesday";
+      break;
+    case 4:
+      day = "thursday";
+      break;
+    case 5:
+      day = "friday";
+      break;
+    case 6:
+      day = "saturday";
+      break;
+    default:
+      console.log("Erroe: current day is equal to: " + currentDay);
+  }
+
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
